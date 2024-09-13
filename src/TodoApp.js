86aaf37279571f63b4f4dd102222f288d8c1e8b6
pareekakhilesh123 <pareekakhilesh5@gmail.com>
2024-09-13 17:22:42 +0530

    complete task fillter And One Button Use Sort

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -4,7 +4,7 @@ import './TodoApp.css';
 function TodoApp() {
   const [inputValue, setInputValue] = useState('');
   const [tasks, setTasks] = useState([]);
-  const [disabledButton, setDisabledButton] = useState('');
+  const [sortorder, setSortOrder] = useState('asc');
 
   const addTask = () => {
     if (inputValue.trim() && tasks.length < 6) {
@@ -22,18 +22,25 @@ function TodoApp() {
     if (event.key === 'Enter')
       addTask();
   };
+  // sort task
+  
+  const incompleteTasks = tasks.filter(task => !task.completed);
+  const completeTasks = tasks.filter(task => task.completed);
 
   const taskSort = () => {
-    const sortedTasks = [...tasks].sort((a, b) => a.text > b.text ? 1 : -1);
-    setDisabledButton('first');
-    setTasks(sortedTasks)
+    const sortedIncompleteTasks = incompleteTasks.sort((a, b) => a.text > b.text ? 1 : -1);
+    const sortedCompleteTasks = completeTasks.sort((a, b) => a.text > b.text ? 1 : -1);
+    setTasks([...sortedIncompleteTasks, ...sortedCompleteTasks]);
+    setSortOrder('asc');
   };
 
   const ztaskSort = () => {
-    const sortedTasks = [...tasks].sort((a, b) => a.text < b.text ? 1 : -1);
-    setDisabledButton('second');
-    setTasks(sortedTasks)
+    const sortedIncompleteTasks = incompleteTasks.sort((a, b) => a.text < b.text ? 1 : -1);
+    const sortedCompleteTasks = completeTasks.sort((a, b) => a.text < b.text ? 1 : -1);
+    setTasks([...sortedIncompleteTasks, ...sortedCompleteTasks]);
+    setSortOrder('dsc');
   };
+  // sort task complete
 
   const deleteTask = (index) => {
     const newTasks = [...tasks];
@@ -73,8 +80,12 @@ function TodoApp() {
             You Can Only Six Task Add
           </span>
         }<br />
-        <button onClick={taskSort} className='sorting' disabled={disabledButton === 'first'}>A-Z Sort</button>
-        <button onClick={ztaskSort} className='sorting' disabled={disabledButton === 'second'}>Z-A Sort</button>
+        <button onClick={taskSort} className='sorting' disabled={sortorder === 'asc'}>A-Z Sort</button>
+        <button onClick={ztaskSort} className='sorting' disabled={sortorder === 'dsc'}>Z-A Sort</button> 
+       
+        <button onClick={sortorder === 'asc' ? ztaskSort : taskSort}>
+        {sortorder === 'asc' ?   '↓' : '↑' }
+      </button>
       </div>
 
       <ul className="task-list">
